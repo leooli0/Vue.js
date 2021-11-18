224663ab879687de0ddae813decf0af6334e1ed0
Vanessa Souto <vanessasoutoc@gmail.com>
2021-11-18 19:46:35 -0300

    vue working

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,4 +2,21 @@
 
 @section('content')
     <vc-users></vc-users>
+    {{-- <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>E-mail</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $u)
+                <tr>
+                    <td>{{ $u->name }}</td>
+                    <td>{{ $u->email }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table> --}}
+
 @stop
