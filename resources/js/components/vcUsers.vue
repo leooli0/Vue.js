c72c220d40ee1f2854b4ec6c972aedce466d1f58
Leonardo Santos Oliveira <leonardosantos300305@gmail.com>
2021-11-19 12:00:15 -0300

    continuacao curso vue

--- resources/js/components/vcUsers.vue
+++ resources/js/components/vcUsers.vue
@@ -1,7 +1,53 @@
 <script>
-export default {};
+export default {
+  props: ['users'],
+
+  data() {
+    return {
+      list: this.users,
+      sortProperty: 'name',
+      sortDirection: 1,
+      filterTerm: ''
+    }
+  },
+
+  methods: {
+    sort (ev, property){
+
+      ev.preventDefault();
+
+      this.sortProperty = property;
+
+      if (this.sortDirection === 1){
+        this.sortDirection = -1;
+      }else {
+        this.sortDirection = 1;
+      }
+    }
+  }
+
+
+};
 </script>
 <template>
-  <div>Falando de dentro do vue</div>
+  <div>
+  <div class="well">
+      <input type="text" class="form-control" placeholder="Filtrar a lista a baixo" v-model="filterTerm">
+  </div>
+     <table class="table table-striped">
+        <thead>
+            <tr>
+                <th><a href="#" @click="sort($event, 'name')">Nome</a></th>
+                <th><a href="#" @click="sort($event, 'email')">E-mail</a></th>
+            </tr>
+        </thead>
+        <tbody> 
+              <tr v-for="u in list">
+                <td>{{ u.name }}</td>
+                <td>{{ u.email }}</td>
+              </tr>
+        </tbody>
+    </table> 
+  </div>
 </template>
 <style scope=""></style>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,22 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <vc-users></vc-users>
-    {{-- <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Nome</th>
-                <th>E-mail</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $u)
-                <tr>
-                    <td>{{ $u->name }}</td>
-                    <td>{{ $u->email }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table> --}}
+
+    <vc-users :users="{{ $users }}"></vc-users>
 
 @stop
